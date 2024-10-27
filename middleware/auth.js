b3aa5dbc479e62deeb2f0c93d5d563240199f4ed
Saul <saulavi21@gmail.com>
2024-10-26 21:10:15 -0400

    fix: login route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,9 @@
 // middleware/auth.js
 
-module.exports = {
-    ensureAuthenticated: (req, res, next) => {
-        if (req.session.userId) {
-            return next();
-        }
-        req.flash('error', 'Please log in to view that resource');
-        res.redirect('/login'); // Redirect unauthenticated users to the login page
+module.exports = function ensureAuthenticated(req, res, next) {
+    if (req.session.userId) {
+        return next();
     }
+    req.flash('error', 'Please log in to view that resource');
+    res.redirect('/auth/login'); // Redirect to login instead of signup
 };
