8871db1d3c2657411b0840e763a0c74a1f1d926f
Saul <saulavi21@gmail.com>
2024-11-20 14:54:31 -0500

    add events field

--- models/Listing.js
+++ models/Listing.js
@@ -1,25 +1,26 @@
 // models/Listing.js
 
 const mongoose = require('mongoose'); 
-const mongoosePaginate = require('mongoose-paginate-v2'); // If using pagination
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const listingSchema = new mongoose.Schema({ 
-    title: { type: String, required: true }, 
-    price: { type: Number, required: true }, 
-    category: { type: String, required: false },
-    location: { type: String, required: false },
-    handmade: { type: String, required: false }, 
-    quantity: { type: Number, default: 1 }, 
-    delivery: { type: String, default: 'pickup' },
-    color: { type: String, default: 'N/A' }, 
-    photos: [{ type: String }], 
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    description: { type: String, required: true }, 
-    created: { type: Date, default: Date.now },
-    condition: { type: String, required: false },
-    isDraft: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+const listingSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: false },
+  location: { type: String, required: false },
+  handmade: { type: String, required: false },
+  quantity: { type: Number, default: 1 },
+  delivery: { type: String, default: "pickup" },
+  color: { type: String, default: "N/A" },
+  photos: [{ type: String }],
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  description: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+  condition: { type: String, required: false },
+  isDraft: { type: Boolean, default: false },
+  event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 // Middleware to update 'updatedAt' before saving
